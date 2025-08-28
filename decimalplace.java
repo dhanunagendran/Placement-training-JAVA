03ca9b70f432291c83b335b4936e016022bde623
Dhanu shree <160223250+dhanunagendran@users.noreply.github.com>
2025-08-28 12:26:57 +0530

    Create decimalplace.java

---
+++ decimalplace.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = a*b;
+        System.out.printf("%.2f", c);
+    }
+}
