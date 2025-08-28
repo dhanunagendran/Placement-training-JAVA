54ebbccb1642365464c56a2d4957ec6d33f85d30
Dhanu shree <160223250+dhanunagendran@users.noreply.github.com>
2025-08-28 12:23:00 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner c = new Scanner(System.in);
+        int a = c.nextInt();
+        int b = c.nextInt();
+        int d = a + b;
+        System.out.println("Sum of " + a + " and " + b + " is " + d);
+    }
+}
