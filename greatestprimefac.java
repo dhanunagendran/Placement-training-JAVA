e2b10a5b4e840242ce053fe687185e4ed8805ad5
Dhanu shree <160223250+dhanunagendran@users.noreply.github.com>
2025-08-28 12:30:43 +0530

    Create greatestprimefac.java

---
+++ greatestprimefac.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String[] args) {
+           Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong(); 
+        long maxPrime = -1;
+
+      while (n % 2 == 0) {
+            maxPrime = 2;
+            n /= 2;
+        }
+
+        for (long i = 3; i * i <= n; i += 2) {
+            while (n % i == 0) {
+                maxPrime = i;
+                n /= i;
+            }
+        }
+
+     
+        if (n > 2) {
+            maxPrime = n;
+        }
+
+        System.out.println(maxPrime);
+        
+        
+    }
+}
